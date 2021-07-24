1231c395485f8363fe433c3d7a56fd4916121df4
bucky2obs <our4000@gmail.com>
2021-07-24 18:24:53 +0900

    Created a new Expo app

--- App.tsx
+++ App.tsx
@@ -1,81 +1,24 @@
-import { StatusBar } from "expo-status-bar";
-import React, { useState, useEffect } from "react";
-import { StyleSheet, View } from "react-native";
-import { Alert } from "react-native";
-import axios from "axios";
-import * as Location from "expo-location";
-import Loading from "./src/components/Loading";
-import Weather from "./src/components/Weather";
-import { delay } from "./assets/common";
+import 'react-native-gesture-handler';
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
 
-interface weather {
-  temp?: number;
-  condition?: string;
-}
+import useCachedResources from './hooks/useCachedResources';
+import useColorScheme from './hooks/useColorScheme';
+import Navigation from './navigation';
 
 export default function App() {
-  const API_KEY = `c31bbb1a4f80b8a1e3e2fab87f11305c`;
-  const [loading, setLoading] = useState(true);
-  const [weather, setWeather] = useState({});
+  const isLoadingComplete = useCachedResources();
+  const colorScheme = useColorScheme();
 
-  const getLocation = async (): Promise<number[] | undefined> => {
-    try {
-      await delay(2000);
-      await Location.requestPermissionsAsync();
-      const {
-        coords: { latitude, longitude },
-      } = await Location.getCurrentPositionAsync();
-      return [latitude, longitude];
-    } catch (error) {
-      Alert.alert("Can't find you ");
-    }
-  };
-
-  const getWeather = async (
-    latitude: number,
-    longitude: number
-  ): Promise<weather | undefined | {}> => {
-    try {
-      const {
-        data: {
-          main: { temp },
-          weather,
-        },
-      } = await axios.get(
-        `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${API_KEY}&units=metric`
-      );
-      return {
-        temp: Math.round(temp),
-        condition: weather[0].main,
-      };
-    } catch (error) {
-      console.log(`Can't get Weather`);
-      return {};
-    }
-  };
-
-  const fetch = async () => {
-    const [latitude, longitude] = await getLocation();
-    const weather = await getWeather(latitude, longitude);
-    console.log(`weather: `, weather);
-
-    setWeather(weather ? weather : {});
-    setLoading(false);
-  };
-
-  useEffect(() => {
-    fetch();
-  }, []);
-
-  return (
-    <View style={styles.container}>
-      {loading ? <Loading /> : <Weather weather={weather} />}
-    </View>
-  );
+  if (!isLoadingComplete) {
+    return null;
+  } else {
+    return (
+      <SafeAreaProvider>
+        <Navigation colorScheme={colorScheme} />
+        <StatusBar />
+      </SafeAreaProvider>
+    );
+  }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
-});
--- assets/common/util/delay.ts
+++
@@ -1,3 +0,0 @@
-export default function delay(delay: number = 1000): Promise<null> {
-  return new Promise((resolve) => setTimeout(resolve, delay));
-}
