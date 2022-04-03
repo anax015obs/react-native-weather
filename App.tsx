47edd3acc63f11d6b937c9e4166415f99d54813f
bucky2obs <our4000@gmail.com>
2022-04-03 21:52:29 +0900

    ..

--- App.tsx
+++ App.tsx
@@ -1,11 +1,18 @@
-import 'react-native-gesture-handler';
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { SafeAreaProvider } from 'react-native-safe-area-context';
+import "react-native-gesture-handler";
+import { StatusBar } from "expo-status-bar";
+import React, { useState } from "react";
+import { SafeAreaProvider } from "react-native-safe-area-context";
+import * as Location from "expo-location";
+import useCachedResources from "./hooks/useCachedResources";
+import useColorScheme from "./hooks/useColorScheme";
+import Navigation from "./navigation";
+import { Alert } from "react-native";
+import axios from "axios";
 
-import useCachedResources from './hooks/useCachedResources';
-import useColorScheme from './hooks/useColorScheme';
-import Navigation from './navigation';
+interface weather {
+  temp?: number;
+  condition?: string;
+}
 
 export default function App() {
   const isLoadingComplete = useCachedResources();
@@ -17,6 +24,7 @@ export default function App() {
     return (
       <SafeAreaProvider>
         <Navigation colorScheme={colorScheme} />
+
         <StatusBar />
       </SafeAreaProvider>
     );
